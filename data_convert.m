c49a23dc8fb4c2fed6e5cef2158a1d43c39c5b0a
zyy123jy <zhuyingying2@gmail.com>
2018-02-21 22:30:51 -0500

    Add files via upload

---
+++ data_convert.m
@@ -0,0 +1,130 @@
+function data = data_convert(dat)
+formatIn = 'mm/dd/yyyy';
+index = 1; % the subject number
+Idx(1) = dat.id(2);
+t = 1;
+i = 2;
+    age(index) = dat.age(i);
+    edu(index) = dat.edu(i);
+    time(index,t) = datenum(dat.date{i},formatIn);
+    T(index,t)= (time(index,t)-time(index,1))/365 + age(index);
+    
+    Y(1,t,index) = dat.MMSE(i);
+    Y(2,t,index) = dat.ADAS(i);
+    Y(3,t,index) = dat.ven(i)/dat.brain(i);
+    Y(4,t,index) = dat.hip(i)/dat.brain(i);
+    Y(5,t,index) = dat.cdrs(i);
+    
+    if length(dat.av45{i})>0
+    Y(6,t,index) = str2double( dat.av45{i});
+    end
+    if length(dat.fdg{i})>0
+    Y(7,t,index) = str2double( dat.fdg{i});
+    end
+    if length(dat.pib{i})>0
+    Y(8,t,index) = str2double( dat.pib{i});
+    end
+    Y(9,t,index) = dat.ptau(i);
+    Y(10,t,index) = dat.abeta(i);
+       
+    label{index,t} = dat.label{i};
+    change(index) = dat.change(i);
+  
+    X(index,1) = dat.APOE(i);
+    if length(dat.gender{i}) == 4
+      X(index,2) = 1;
+    else
+      X(index,2) = 0;
+    end
+    X(index,3) = dat.edu(i);
+display(Idx)
+for i = 3:length(dat.date)
+    if i == 3505
+        continue;
+    end
+    if dat.id(i)==Idx(index) 
+    t = t + 1;
+    X(index,1) = dat.APOE(i);
+    display(i)    
+    if length(dat.gender{i}) == 4
+      X(index,2) = 1;
+    else
+      X(index,2) = 0;
+    end
+    X(index,3) = dat.edu(i);
+    age(index) = dat.age(i);
+    edu(index) = dat.edu(i);
+    time(index,t) = datenum(dat.date{i},formatIn);
+    T(index,t)= (time(index,t)-time(index,1))/365 + age(index);
+  
+    Y(1,t,index) = dat.MMSE(i);
+    Y(2,t,index) = dat.ADAS(i);
+    Y(3,t,index) = dat.ven(i)/dat.brain(i);
+    Y(4,t,index) = dat.hip(i)/dat.brain(i);
+    Y(5,t,index) = dat.cdrs(i);
+    
+    if length(dat.av45{i})>0
+    Y(6,t,index) = str2double( dat.av45{i});
+    end
+    if length(dat.fdg{i})>0
+    Y(7,t,index) = str2double( dat.fdg{i});
+    end
+    if length(dat.pib{i})>0
+    Y(8,t,index) = str2double( dat.pib{i});
+    end
+    Y(9,t,index) = dat.ptau(i);
+    Y(10,t,index) = dat.abeta(i);   
+    label{index,t} = dat.label{i};
+    change(index) = dat.change(i);
+    
+    else  
+    t = 1;       
+    index = index+1;        
+    Idx(index) = dat.id(i);
+    age(index) = dat.age(i);
+    edu(index) = dat.edu(i);
+    time(index,t) = datenum(dat.date{i},formatIn);
+    T(index,t)= (time(index,t)-time(index,1))/365 + age(index);
+    Y(1,t,index) = dat.MMSE(i);
+    Y(2,t,index) = dat.ADAS(i);
+    Y(3,t,index) = dat.ven(i)/dat.brain(i);
+    Y(4,t,index) = dat.hip(i)/dat.brain(i);
+    Y(5,t,index) = dat.cdrs(i);
+    
+    if length(dat.av45{i})>0
+    Y(6,t,index) = str2double( dat.av45{i});
+    end
+    if length(dat.fdg{i})>0
+    Y(7,t,index) = str2double( dat.fdg{i});
+    end
+    if length(dat.pib{i})>0
+    Y(8,t,index) = str2double( dat.pib{i});
+    end
+  
+    Y(9,t,index) = dat.ptau(i);
+    Y(10,t,index) = dat.abeta(i);
+    label{index,t} = dat.label{i};
+    change(index) = dat.change(i);
+     X(index,1) = dat.APOE(i);
+    if length(dat.gender{i}) == 4
+      X(index,2) = 1;
+    else
+      X(index,2) = 0;
+    end
+      X(index,3) = dat.edu(i);
+    end   
+end
+
+
+Y(isnan(Y)) = 0;
+X(isnan(X)) = 0;
+data.X = X';
+data.age = age';
+data.time = time';
+data.Y = Y;
+data.idx = Idx';
+data.label = label';
+data.T = T';
+data.change = change;
+data.edu = edu;
+
